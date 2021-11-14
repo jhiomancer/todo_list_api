85a0dc9e4927ff5a45c3d0bea97caf00c78240aa
jhiomancer <94284125+jhiomancer@users.noreply.github.com>
2021-11-15 03:25:42 +0800

    Added queries to handle username/password validation, get users query

--- api/modules/users/users.sql.queries.js
+++ api/modules/users/users.sql.queries.js
@@ -1,6 +1,8 @@
 const SQL_QUERIES = {
     CREATE_USER: "insert into users (user_username, user_password) values (?,?)",
-    GET_USER_BY_USERNAME: "select user_username from users where user_username = ?"
+    GET_USER_BY_USERNAME: "select user_username from users where user_username = ?",
+    GET_USER_PASSWORD : "select * from users where user_username = ?",
+    GET_ALL_USERS : "select user_id as id,user_username as username,user_create_dt as create_dt from users"
 }
 
 const registerUserQuery = (db,user) => {
@@ -8,9 +10,16 @@ const registerUserQuery = (db,user) => {
     return db.query(SQL_QUERIES.CREATE_USER,p);
 }
 
-
 const getUserByUsernameQuery = (db,username) =>{
     return db.query(SQL_QUERIES.GET_USER_BY_USERNAME,[username]);
 }
 
-module.exports = {registerUserQuery,getUserByUsernameQuery};
+const getUserPasswordByUsernameQuery = (db,username) =>{
+    return db.query(SQL_QUERIES.GET_USER_PASSWORD,[username]);
+}
+
+const getUsersQuery = (db) => {
+    return db.query(SQL_QUERIES.GET_ALL_USERS);
+}
+
+module.exports = {registerUserQuery,getUserByUsernameQuery, getUserPasswordByUsernameQuery, getUsersQuery};
