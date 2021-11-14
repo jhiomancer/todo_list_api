e9f806ec46551fbe7ece6c094482ca999ce1de37
jhiomancer <94284125+jhiomancer@users.noreply.github.com>
2021-11-15 03:22:41 +0800

    Added a validation if the username exists, hashed the passwords. New getUsers and loginUser function. Login Working with access token.

--- api/modules/users/users.functions.js
+++ api/modules/users/users.functions.js
@@ -1,13 +1,14 @@
-const {registerUserQuery, getUserByUsernameQuery} = require("./users.sql.queries");
+require('dotenv').config()
+const { registerUserQuery, getUserByUsernameQuery, getUserPasswordByUsernameQuery, getUsersQuery } = require("./users.sql.queries");
+const bcrypt = require("bcryptjs");
+const { sign }  = require("jsonwebtoken");
 
 const registerUser = async (req,res) => {
     try {
         const db = req.app.get("db");
         const {username,password} = req.body;
-        
         const getUserName = await getUserByUsernameQuery(db,username)
         const checkIfUsernameExists = Object.values(JSON.parse(JSON.stringify(getUserName)));
-
         for (let i = 0; i < checkIfUsernameExists.length; i++) { 
           if (checkIfUsernameExists[i].user_username === username) {
               return res.status(500).json({
@@ -17,7 +18,8 @@ const registerUser = async (req,res) => {
           }
           break;
         }
-        const response = await registerUserQuery(db,{user_username:username,user_password:password});
+        const hashedPassword = await bcrypt.hash(password,10);
+        const response = await registerUserQuery(db,{user_username:username,user_password:hashedPassword});
         if(response.insertId){
             return res.status(200).json({
                 success: true,
@@ -32,9 +34,69 @@ const registerUser = async (req,res) => {
     } catch (e) {
         return res.status(500).json({
             success: false,
-            message: "Somethings went wrong"
+            message: "Something went wrong"
         })
     }
 }
 
-module.exports = registerUser;
+const getUsers = async(req,res) =>{
+    try {
+        let db = req.app.get("db");
+        let response = await getUsersQuery(db);
+        if(response.length > 0){
+            return res.json({
+                success:true,
+                data: response
+            })
+        }else{
+            return res.status(500).json({
+                success: false,
+                message: "Something went wrong"
+            })
+        }
+    } catch (e) {
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong"
+        })
+    }
+}
+
+const loginUser = async (req,res) => {
+    try {
+        const db = req.app.get("db");
+        const {username,password} = req.body;
+        const checkUser = await getUserPasswordByUsernameQuery(db,username);
+        console.log(checkUser)
+        if(checkUser.length == 0){
+            return res.status(500).json({
+                success:false,
+                message:"User doesn't exist"
+            })
+        }else{
+            const isPasswordCorrect = await bcrypt.compare(password,checkUser[0].user_password);
+            if(!isPasswordCorrect){
+                return res.status(500).json({
+                    success:false,
+                    message:"Invalid Username or Password"
+                })
+            }else{
+                const jwtToken = sign({username:checkUser[0].user_username,userId:checkUser[0].user_id},process.env.JWT_KEY);
+                return res.status(200).json({
+                    success:true,
+                    message:"User Login Successful",
+                    accessToken: jwtToken
+                })
+            }
+        }
+    } catch (e) {
+        return res.status(500).json({
+            success:false,
+            message:"Something went wrong"
+        })
+    }
+}
+
+
+
+module.exports = { registerUser, loginUser, getUsers };
