7b0345f477b028269bef7dc2ad98363026d8236d
jhiomancer <94284125+jhiomancer@users.noreply.github.com>
2021-11-15 03:15:35 +0800

    Bug_process.env_unable_to_access_by_application

--- api/conf/db_conn.js
+++ api/conf/db_conn.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const mysql = require("mysql");
 const util = require("util");
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'todo_list'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 pool.query = util.promisify(pool.query);
