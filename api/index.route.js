8ad6e73d5ca23f0bc4c655f95bf80690511ebc4e
jhiomancer <94284125+jhiomancer@users.noreply.github.com>
2021-11-17 02:58:40 +0800

    Added todoRouter for todo endpoint

--- api/index.route.js
+++ api/index.route.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const userRouter = require("./modules/users/users.route");
+const todoRouter = require("./modules/todo/todo.route");
 
 router.use("/user",userRouter);
+router.use("/todo",todoRouter);
 module.exports = router;
